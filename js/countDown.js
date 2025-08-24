eb8b7948a308604545b344e2b6ea8030d00f6563
Raphael <raphaelarzoin173@gmail.com>
2025-08-24 13:47:45 +0200

    changement de preview

--- js/countDown.js
+++ js/countDown.js
@@ -1,5 +1,5 @@
 // Date du mariage (année, mois - 1, jour)
-var weddingDate = new Date(2025, 3, 23); // Mois est 0-indexé (0 = janvier, 1 = février, etc.)
+var weddingDate = new Date(2025, 12, 23); // Mois est 0-indexé (0 = janvier, 1 = février, etc.)
 
 // Fonction pour mettre à jour le compteur
 function updateCountdown() {
